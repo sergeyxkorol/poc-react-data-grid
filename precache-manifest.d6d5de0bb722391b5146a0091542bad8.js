0dbfa60b55ac88b921c13cd7ad50e8abb4bd3c0e
Siarhei_Karol <siarhei_karol@epam.com>
2020-02-06 16:51:17 +0300

    Updates

---
+++ precache-manifest.d6d5de0bb722391b5146a0091542bad8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7471d58621598fe03478ddc5bea8af7",
+    "url": "/poc-react-data-grid/index.html"
+  },
+  {
+    "revision": "941bf17c1d4c8a3761d3",
+    "url": "/poc-react-data-grid/static/css/2.6dcfb7b0.chunk.css"
+  },
+  {
+    "revision": "fc0f6b7ed120aa4bb669",
+    "url": "/poc-react-data-grid/static/css/main.949c266d.chunk.css"
+  },
+  {
+    "revision": "941bf17c1d4c8a3761d3",
+    "url": "/poc-react-data-grid/static/js/2.79bb171d.chunk.js"
+  },
+  {
+    "revision": "68ba486219dc7972526df1416a899782",
+    "url": "/poc-react-data-grid/static/js/2.79bb171d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0f6b7ed120aa4bb669",
+    "url": "/poc-react-data-grid/static/js/main.b047b994.chunk.js"
+  },
+  {
+    "revision": "0fda3094b84d61190a04",
+    "url": "/poc-react-data-grid/static/js/runtime-main.59afde99.js"
+  }
+]);
