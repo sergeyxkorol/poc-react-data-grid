0c106e57691f2e81e9f45ab29e9618974c47aac7
Siarhei_Karol <siarhei_karol@epam.com>
2020-02-06 14:22:19 +0300

    Add columns resizing

--- src/App.js
+++ src/App.js
@@ -3,36 +3,48 @@ import Table from "./components/Table";
 import "./App.scss";
 
 function App() {
-  const [columns, setColumns] = useState([]);
   const [rows, setRows] = useState([]);
   const [errors, setErrors] = useState(false);
 
   function fetchData() {
     fetch("./data.json")
       .then(res => res.json())
-      .then(res => {
-        setRows(res);
-      })
-      .then(() => {
-        const columns = [
-          { key: "id", name: "ID", frozen: true, width: 50 },
-          { key: "first_name", name: "First name" },
-          { key: "last_name", name: "Last name" },
-          { key: "email", name: "Email" },
-          { key: "phone", name: "Phone" },
-          { key: "city", name: "City" },
-          { key: "country", name: "Country" },
-          { key: "country_code", name: "Country code" }
-        ];
-
-        setColumns(columns);
-      })
+      .then(res => setRows(res))
       .catch(err => setErrors(err));
   }
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const columns = [
+    {
+      key: "id",
+      name: "ID",
+      frozen: true,
+      width: 40,
+      sortDescendingFirst: true
+    },
+    {
+      key: "first_name",
+      name: "First name",
+      resizable: true
+    },
+    {
+      key: "last_name",
+      name: "Last name",
+      resizable: true
+    },
+    { key: "email", name: "Email", resizable: true },
+    { key: "phone", name: "Phone", resizable: true },
+    { key: "city", name: "City", resizable: true },
+    { key: "country", name: "Country", resizable: true },
+    {
+      key: "country_code",
+      name: "Country code",
+      resizable: true
+    }
+  ];
 
   return (
     <div className="App">
